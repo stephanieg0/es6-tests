4c7e29f01988b7e80c577bc42b87d42b27fb6cc9
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-01-25 14:37:29 -0600

    first template string test and added engine version into package.json

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -1,8 +1,9 @@
 var expect = require('chai').expect;
 
-describe('test suite', function () {
-  it('true should be true', function () {
-    expect(true).to.be.true;
-  })
+describe('template strings', function () {
+  it('should be a string', function () {
+    var string = `abc`;
+    expect(string).to.be.a('string');
+  });
 
 });

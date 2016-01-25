760f09e2cd99a91b3c395c8ca85eb8dfe112f143
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-01-25 15:29:07 -0600

    added expression and multiline tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -6,4 +6,28 @@ describe('template strings', function () {
     expect(string).to.be.a('string');
   });
 
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo ${bar}`;
+    expect(string).to.equal('foo baz');
+  });
+
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal('The date is: ' + date);
+  });
+
+  it('evaluate expressions', function () {
+    var sum = `${1+2}`;
+    var string = `one + two is: ${sum}`;
+    expect(string).to.equal(`one + two is: ` + sum);
+  });
+
+  it('handle multiline strings', function () {
+    var string = `hello
+    world`;
+    expect(string).to.equal(`hello\n    world`);
+  });
+
 });
